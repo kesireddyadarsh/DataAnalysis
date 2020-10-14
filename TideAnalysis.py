4ff1b29b60f90a0b4bf97b282f0bc49ab6bd487f
adarsh kesireddy <adarshkesireddy@adarshs-MacBook-Pro.local>
2020-10-14 18:52:04 -0500

    need some more work

--- TideAnalysis.py
+++ TideAnalysis.py
@@ -10,7 +10,21 @@ def fillgaps(frames_to_use):
     # print(frames_to_use)
     # frames_to_use.fillna(0.000)
     # frames_to_use.stack().apply(pd.to_numeric, errors='ignore').fillna(0).unstack()
-    frames_to_use['data']=frames_to_use['data'].where(frames_to_use['data'].between(6,12))
+    frequency = 240
+    while frequency < len(frames_to_use):
+        working_frame = frames_to_use.head(frequency)
+        list_of_names = working_frame['data'].to_list()
+        for i in range(0, len(list_of_names)):
+            print(i)
+        frequency += 240
+
+
+    # for frequency  in :
+    #     print(i, j)
+    #     frames_to_use['date_time'].replace({i: i.replace('T', ' ')})
+    #     print(frames_to_use['date_time'][0])
+
+    frames_to_use['data'] = frames_to_use['data'].where(frames_to_use['data'].between(6,12))
 
     # frames_to_use['200-pwl'] = frames_to_use['200-pwl'].replace(np.nan, 0) #working
     # frames_to_use.to_csv(r'/home/ak/Documents/DataAnalysis/DataAnalysis/File_Name.csv', index = False)
@@ -41,21 +55,21 @@ if __name__ == '__main__':
     data_nueces_url = 'https://tamucc-ir.tdl.org/bitstream/handle/1969.6/87766/NuecesBay_RawDataOnly.csv?sequence=2&isAllowed=y'
     data_sargent = pd.read_csv(data_sargent_url)
     data_nueces = pd.read_csv(data_nueces_url)
-    data_sargent.rename(columns = {'200-pwl':'data'}, inplace = True)
-    data_nueces.rename(columns = {'185-pwl':'data'}, inplace = True)
+    data_sargent.rename(columns = {'#date+time': 'date_time', '200-pwl':'data'}, inplace = True)
+    data_nueces.rename(columns = {'#date+time': 'date_time', '185-pwl':'data'}, inplace = True)
     """
     Uncomment below code to open raw data
     """
     # plot_row_data_sargent(data_sargent) #plot generic data
     # plot_row_data_nueces(data_nueces) #plot generic data
-    plot_row_data(data_sargent,data_nueces)
+    # plot_row_data(data_sargent, data_nueces)
     """
     Cleaning the data:
         1. Remove outliners
         2. Find the mean of given time and place it
     """
     # print(data_nueces.loc[67]) #to access location
-    # fillgaps(data_sargent)
+    fillgaps(data_sargent)
     # fillgaps(data_nueces)
     # plot_row_data_sargent(data_sargent)
     # plot_row_data_sargent(data_nueces)

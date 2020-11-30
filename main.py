6ddd703cf3fdc12a6bec429f89b698cda293de4e
kesireddyadarsh <kesireddy.k.adarsh@gmail.com>
2020-11-30 17:07:05 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -9,47 +9,69 @@ from scipy import signal
 """
 Case_number -- 0 if imported data; 1 for noise data and imported data
 """
-def plot_initial_value(case_number):
-    if(case_number == 0):
-        plt.scatter(target_location_x,target_location_y,s=300,marker='o',c='green',label ='Target Location')
-        plt.scatter(obstacle_location_x,obstacle_location_y,s=300,c='red',label = 'Obstacle Location')
-        plt.plot(path_x,path_y,c='navy',label = 'True Path')
-        plt.xlabel('X Coordinates')
-        plt.ylabel('Y Coordinates')
-        plt.show()
-    elif(case_number == 1):
-        fig, ax = plt.subplots()
-        ax.scatter(target_location_x,target_location_y,s=300,marker='o',c='green',label ='Target Location')
-        ax.scatter(obstacle_location_x,obstacle_location_y,s=300,c='red',label = 'Obstacle Location')
+def plot_initial_value(case_number,path_x,path_y,noise_path_x):
+    fig, ax = plt.subplots()
+    ax.scatter(target_location_x,target_location_y,s=300,marker='o',c='green',label ='Target Location')
+    ax.scatter(obstacle_location_x,obstacle_location_y,s=300,c='red',label = 'Obstacle Location')
+    if case_number == 1:
         ax.plot(noise_path_x,path_y,c="skyblue", label ='Noise Path')
-        ax.plot(path_x,path_y,c='navy',label = 'True Path')
-        plt.xlabel('X Coordinates')
-        plt.ylabel('Y Coordinates')
-        plt.legend(loc='upper right',markerscale=0.75)
-        plt.show()
+    ax.plot(path_x,path_y,c='navy',label = 'True Path')
+    plt.xlabel('X Coordinates')
+    plt.ylabel('Y Coordinates')
+    plt.legend(loc='upper right',markerscale=0.75)
+    plt.show()
 
 """
 Path coordinates are of size 7519
 Use 5000 data points for training
+Reward 4500 
 """
 if __name__ == '__main__':
     # print("This is working")
     url = "/home/ak/Documents/DataAnalysis/Project/data.csv"
-    data = pd.read_csv(url)
-    data_x = data.loc[:,'x']
-    data_y = data.loc[:,'y']
-    path_x = data_x.values
-    path_y = data_y.values
+    url_1 = "/home/ak/Documents/DataAnalysis/Project/data_1.csv"
+
+    data_training = pd.read_csv(url)
+    data_x_training = data_training.loc[:,'x']
+    data_y_training = data_training.loc[:,'y']
+    path_x_training = data_x_training.values
+    path_y_training = data_y_training.values
+
+    data_test = pd.read_csv(url_1)
+    data_x_test = data_test.loc[:,'x']
+    data_y_test = data_test.loc[:,'y']
+    path_x_test = data_x_test.values
+    path_y_test = data_y_test.values
+
     target_location_x = np.array([5, 40, 95, 95, 95])
     target_location_y = np.array([95, 95, 95, 40, 5])
     obstacle_location_x = np.array([22, 60, 95, 95, 50, 5, 60, 20, 75, 30])
     obstacle_location_y = np.array([95, 95, 60, 22, 50, 25, 5, 65, 65, 30])
     #plot_initial_value()
-    noise = np.random.uniform(-3,3,len(path_x))
-    if len(noise) < len(path_x):
-        noise_path_x = path_x.copy()
-        noise_path_x[:len(noise)] += noise
+
+    # noise = np.random.uniform(-1,1,len(path_x_training))
+    noise = np.random.normal(size=(len(path_x_training)))
+    print(len(noise),noise)
+    plt.plot(noise)
+    plt.show()
+    if len(noise) < len(path_x_training):
+        noise_path_x_training = path_x_training.copy()
+        noise_path_x_training[:len(noise)] += noise
     else:
-        noise_path_x = noise.copy()
-        noise_path_x[:len(path_x)] += path_x
-    plot_initial_value(1)
+        noise_path_x_training = noise.copy()
+        noise_path_x_training[:len(path_x_training)] += path_x_training
+
+    if len(noise) < len(path_x_test):
+        noise_path_x_test = path_x_test.copy()
+        noise_path_x_test[:len(noise)] += noise
+    else:
+        noise_path_x_test = noise.copy()
+        noise_path_x_test[:len(path_x_test)] += path_x_test
+
+    if len(noise_path_x_test) != len(path_y_test):
+        path_y_test = np.delete(path_y_test,len(noise_path_x_test)-1)
+
+
+    # plot_initial_value(1,path_x_training,path_y_training,noise_path_x_training)
+    plot_initial_value(1,path_x_test,path_y_test,noise_path_x_test)
+
